333c0a0f450633c6670ad70cc07ad508da6d82f8
K11N4L2 <59125204+K11N4L2@users.noreply.github.com>
2020-02-02 17:01:11 +0000

    Create speedconverter.py

---
+++ speedconverter.py
@@ -0,0 +1,14 @@
+print('Welome to the MPH to MPS Conversion App')
+
+mph = float(input('\nWhat is your speed in meters per hour: '))
+
+mps = mph*.4474
+mps = round(mps, 2)
+
+
+# print(mph)
+print('Your speed in meters per second ' + str(mps))
+
+# print('\nHow far are you traveling?')  # use mph
+
+# distance = input('\nPlease enter how many meters')
